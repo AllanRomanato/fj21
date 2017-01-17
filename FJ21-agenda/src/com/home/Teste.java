e4b39a2d447e4b29cb63a764f65c37613704b90b
allanromanato <allanromanato@localhost>
2017-01-17 14:48:55 -0200

    Segundo Commit

--- FJ21-agenda/src/com/home/Teste.java
+++ FJ21-agenda/src/com/home/Teste.java
@@ -1,6 +1,8 @@
 package com.home;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,32 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Teste
  */
-@WebServlet("/Teste")
+@WebServlet("/oi")
 public class Teste extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Teste() {
-        super();
-        // TODO Auto-generated constructor stub
+    @Override
+    protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	PrintWriter writer = arg1.getWriter();
+    	writer.println("TEste de Servlet e depois commit");
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
